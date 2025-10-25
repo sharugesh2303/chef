bd3dcd0ce6bfdb7b3009912e0bd457501a0a4faa
sharugesh <sharugesh39@gmail.com>
2025-10-25 21:03:51 +0530

    chore: Force Vercel to read new env variable

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import App from './App.jsx';
 import './index.css';
 
 // Force Vercel to make a new file again
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-// Add this line
+// // Add this line
+// And this line to be 100% sure it's a new change
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
