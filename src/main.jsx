b6717899a394340408999de2d2ed9f38cec701b1
sharugesh <sharugesh39@gmail.com>
2025-10-25 16:45:17 +0530

    Fix: Add BrowserRouter to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // 👈 Import the new authentication wrapper
+import { BrowserRouter } from 'react-router-dom'; // 👈 1. Import this
+import App from './App.jsx';
 import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> {/* 👈 Render the new App component */}
+    <BrowserRouter> {/* 👈 2. Wrap your App component */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
